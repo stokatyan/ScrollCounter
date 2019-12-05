e3fc8a5004c4a4feb3df1efb726ea44e1ce7d693
Shant Tokatyan <shant.tokatyan@dexcom.com>
2019-12-04 18:48:09 -0800

    Initial Commit

---
+++ ScrollCounter/ScrollCounter.h
@@ -0,0 +1,19 @@
+//
+//  ScrollCounter.h
+//  ScrollCounter
+//
+//  Created by Shant Tokatyan on 12/4/19.
+//  Copyright © 2019 Stokaty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScrollCounter.
+FOUNDATION_EXPORT double ScrollCounterVersionNumber;
+
+//! Project version string for ScrollCounter.
+FOUNDATION_EXPORT const unsigned char ScrollCounterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollCounter/PublicHeader.h>
+
+
